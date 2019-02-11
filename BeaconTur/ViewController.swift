a69b162fe3ad6d477ad3171fc8caec91a98d9eed
TranLuongTuanAnh <tlta.bkhn@gmail.com>
2019-02-11 16:13:45 +0900

    Initial Commit

---
+++ BeaconTur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconTur
+//
+//  Created by anh tranluongtuan on 2019/02/11.
+//  Copyright © 2019 anh tranluongtuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
